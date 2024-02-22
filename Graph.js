9b05d9e68a10510c17694d70e8a576017cbd7cd0
Kasiperumal Achappan <kasiperumal@users.noreply.github.com>
2024-02-22 22:37:51 +0530

    Create Graph.js

---
+++ Graph.js
@@ -0,0 +1,7 @@
+    useEffect(() => {
+        // Call handleSearch when the component mounts
+        handleSearch();
+    }, []); // Empty dependency array ensures this runs once on mount
+
+  const handleSearch = () => {
+    // e.preventDefault();
